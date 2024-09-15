be6d368d1a38e771b9e1df732683311e8c260382
thetygerpotatto <letras.cj@gmail.com>
2024-09-15 15:02:45 -0500

    Nuevos test añadidos a la clase MaxLista

--- app/src/test/scala/taller/MaxListaTest.scala
+++ app/src/test/scala/taller/MaxListaTest.scala
@@ -20,15 +20,31 @@ class MaxListaTest extends AnyFunSuite {
     assert(objmax.MaxLin(List(1, 9, 2, 3, 4, 5, 8, 15, 15)) == 15)
   }
 
+  test("devolver 20 en List(0, 2, 0, 0, 6, 20)") {
+    assert(objmax.MaxLin(List(0, 2, 0, 0, 6, 20)) == 15)
+  }
+  
+  test("devolver 102 en List(100, 99, 88, 102, 97)") {
+    assert(objmax.MaxLin(List(100, 99, 88, 102, 97)) == 15)
+  }
+  
   test("devolver 1 en List(1)") {
     assert(objmax.MaxIt(List(1)) == 1)
   }
 
-  test("devolver 3 en List(3)") {
-    assert(objmax.MaxIt(List(3)) == 3)
+  test("devolver 1000 en List(3, 4, 5, 8, 1000)") {
+    assert(objmax.MaxIt(List(3, 4, 5, 8, 1000)) == 3)
   }
 
   test ("Devolver 500 en List(100, 200, 300, 400, 500)") {
+    assert(objmax.MaxIt(List(100, 200, 300, 400, 500)) == 500)
+  }
+  
+  test ("Devolver 3 en List(1, 1, 1, 1, 3)") {
+    assert(objmax.MaxIt(List(100, 200, 300, 400, 500)) == 500)
+  }
+  
+  test ("Devolver 500 en List(0, 65, 32, 27, 500)") {
     assert(objmax.MaxIt(List(100, 200, 300, 400, 500)) == 500)
   }
 }
